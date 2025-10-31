d63a7fd5ea653b7c3e6fb276dc1dd48224bacf40
failed33 <125191796+failed33@users.noreply.github.com>
2025-10-31 17:51:27 +0100

    deprecated Swift-FHIR package and replaced by apple native

--- Package.swift
+++ Package.swift
@@ -16,17 +16,20 @@ let package = Package(
             targets: ["SMART"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/smart-on-fhir/Swift-FHIR", "4.2.0"..<"5.0.0"),
+		//.package(url: "https://github.com/smart-on-fhir/Swift-FHIR", "4.2.0"..<"5.0.0"),
 		.package(url: "https://github.com/p2/OAuth2", "5.1.0"..<"6.0.0"),
+        .package(url: "https://github.com/apple/FHIRModels.git",
+                 .upToNextMajor(from: "0.7.0"))
     ],
     targets: [
 		.target(
 			name: "SMART",
 			dependencies: [
-				.product(name: "OAuth2"),
-				.product(name: "FHIR"),
+				.product(name: "OAuth2", package: "OAuth2"),
+				.product(name: "ModelsR5", package: "FHIRModels"),
 			],
 			path: "Sources",
 			sources: ["SMART", "Client", "iOS", "macOS"]),
     ]
 )
+
